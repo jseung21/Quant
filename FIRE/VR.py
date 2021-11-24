f910ed1fe6d239d4dbf78e330f59995c81696909
Jaeseung Park <jaeseung.p@gmail.com>
2021-11-24 20:36:10 +0900

    Update VR.py

--- FIRE/VR.py
+++ FIRE/VR.py
@@ -36,25 +36,24 @@ pool_cash = 0
 ########################################################
 
 
-def value_rebalansing(trade_data) :
+def value_rebalansing(vr_score, day_score) :
     
     
     # pool은 V의 60%를 넘지 않는다 <-- 이렇게 되도록 매매 한다
 
     _next_expect_value = 0 
-    _current_expect_value = 0
-    _current_evalution = 0
+    _current_expect_value = vr_cycle.cur_expt_val.values[-1]
+    # 현재평가금
+    _current_evalution = day_score.cur_eval.values[-1]
     _rise_ratio = 0
-    _pool = 0
+    _pool = day_score.cash.values[-1]
     _next_min_value = 0
     _next_max_value = 0
-
-    # TODO : 값설정
-    
 
 
     # 상승률 찾기 = pool/현재V기대치, 그리고 현재평가금과 현재V와 비교
     _rise_ratio_tmp = _pool / _current_expect_value
+    # 기대치보다 적게 오른 경우, 현재평가금 < 현재기대치
     if _current_evalution < _current_expect_value :
         _rise_ratio = 1 + (_rise_ratio_tmp / 10)
     else :
@@ -70,68 +69,97 @@ def value_rebalansing(trade_data) :
     _next_max_value = _next_expect_value * 1.25
 
 
+    # 날짜와 현재 평가액 세팅
+    vr_score.date.values[-1] = day_score.date.values[-1]
+    vr_score.cur_eval.values[-1] = day_score.cur_eval.values[-1]
+    
+    new_vr_score = {'date' : ''
+                    ,'stock_nm' : stock
+                    ,'cur_eval' : ''
+                    ,'cur_expt_val' : _next_expect_value
+                    ,'min_val' : _next_min_value
+                    ,'max_val' : _next_max_value
+                    ,'trade_vol' : ''
+                    ,'pool' : ''}
+    vr_score = vr_score.append(new_vr_score, ignore_index=True)
 
+
+
+# init
 for stock in stock_list :
 
     # 주식 데이터 가지고 오기
     trade_data = yf.download(stock,start = back_test_start_date)
     trade_data = trade_data.reset_index()
+#                Date        Open        High         Low       Close   Adj Close    Volume
+# 0    2010-02-11    0.813750    0.869792    0.811146    0.865104    0.862942   1728000
+# 1    2010-02-12    0.841563    0.876146    0.836667    0.868646    0.866475   4300800
+# 2    2010-02-16    0.889063    0.904375    0.875104    0.902292    0.900037   4809600
+# 3    2010-02-17    0.914375    0.917813    0.900625    0.917604    0.915311   9590400
+
 
     # 거래 사이클 
-    _trade_cycle = 1
+    _trade_cycle_cnt = 1
 
 
-    # 컬럼 : 날짜, 현재평가금, 현재V기대치, 최소값, 최대값, 거래액, POOL
-    # columns : cur_eval, cur_expt_val, min_val, max_val, trade_vol, pool
-    score = pd.DataFrame({'date' : [back_test_start_date]
-                        ,'cur_eval' : [0]
-                        ,'cur_expt_val' : [0]
-                        ,'min_val' : [0]
-                        ,'max_val' : [0]
-                        ,'trade_vol' : [0]
-                        , 'pool' : [0]})
+    # 컬럼 : 날짜, 주식명,  현재평가금, 현재V기대치, 최소값, 최대값, 거래액, POOL
+    # columns : date, stock_nm, cur_eval, cur_expt_val, min_val, max_val, trade_vol, pool
+    vr_score = pd.DataFrame(columns=['date','stock_nm','cur_eval','cur_expt_val','min_val','max_val','trade_vol','pool'])
+    # vr_score = pd.DataFrame({'date' : [back_test_start_date]
+    #                      ,'stock_nm' : [stock]
+    #                      ,'cur_eval' : [0]
+    #                      ,'cur_expt_val' : [0]
+    #                      ,'min_val' : [0]
+    #                      ,'max_val' : [0]
+    #                      ,'trade_vol' : [0]
+    #                      ,'pool' : [init_cash]})
+
+    day_score = pd.DataFrame(columns=['date','stock_nm','cur_px','hold_vol','cur_eval','cash'])
+
 
 
 
     # 테스트 기간만큼 실행
     for i in range(test_priod) :
 
-        # Value Rebalansing
-        if _trade_cycle == 1 and _trade_cycle % vr_cycle == 0 :
+        # 첫날 거래
+        if i == 0 :
+            _date = trade_data.Date[i]
+            _cur_px = trade_data.Close[i]
+            _hold_vol = init_cash // _cur_px
+            _cur_eval = _cur_px * _hold_vol
+            new_day_score = {'date' : _date
+                           ,'stock_nm' : stock
+                           ,'cur_px' : _cur_px
+                           ,'hold_vol' : _hold_vol
+                           ,'cur_eval' : _cur_eval
+                           ,'cash' : init_cash - _cur_eval}
+            day_score = day_score.append(new_day_score, ignore_index=True)
             
-            value_rebalansing(trade_data)
-
+            new_vr_score = {'date' : _date
+                            ,'stock_nm' : stock
+                            ,'cur_eval' : _cur_eval
+                            ,'cur_expt_val' : init_cash
+                            ,'min_val' : 0
+                            ,'max_val' : 0
+                            ,'trade_vol' : 0
+                            ,'pool' : init_cash - _cur_eval}
+            vr_score = vr_score.append(new_vr_score, ignore_index=True)
+            
+            
+            value_rebalansing(vr_score, day_score)
+            
+            
+        # TODO : Day 거래
         else :
-            1=1
+            1 ==1
 
 
+        # Value Rebalansing
+        if _trade_cycle_cnt % vr_cycle == 0 :
+            
+            value_rebalansing(vr_score, trade_data)
 
-
+  
         # 다음 trade
-        _trade_cycle += 1
-
-        # 거래 사이클 보다 작을 경우
-        # if _trade_cycle < trade_cycle :
-
-            # 당일 주가 확인 후 거래
-            # 현재 평가 금액
-
-            # V 기대치
-
-            # 최소 
-
-            # 최대
-
-
-
-            ### 다음V = 현재V * 상승률 + pool
-
-
-        # else :
-        #     # 금액 투입
-        #     pool_cash += input_money_per_trade
-
-        #     # LOC 매도/매수 설정
-
-        _date = data.Date[i]
-        _cur_px = data['Close'][i]
+        _trade_cycle_cnt += 1
