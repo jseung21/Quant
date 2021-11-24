739850748e6f7e9856b1d99776bec1d1c4574e34
Jaeseung Park <jaeseung.p@gmail.com>
2021-11-24 20:35:39 +0900

    Update Mumae.py

--- FIRE/Mumae.py
+++ FIRE/Mumae.py
@@ -6,32 +6,34 @@ import openpyxl
 
 
 # 종목
-stock_list = ['TQQQ','SOXL','TNA','FNGU','UDOW'
-             ,'FAS','SPXL','UPRO','LABU','YINN'
-             ,'DFEN','TECL','BNKU','TMF','DRN'
-             ,'URTY','DPST','NAIL','NRGU','DUSL']
+# stock_list = ['TQQQ','SOXL','TNA','FNGU','UDOW'
+#              ,'FAS','SPXL','UPRO','LABU','YINN'
+#              ,'DFEN','TECL','BNKU','TMF','DRN'
+#              ,'URTY','DPST','NAIL','NRGU','DUSL']
 
 
 ###################### 값 설정 #########################
 # 해당 주식
-# stock_list = ['TQQQ']
+stock_list = ['BULZ','LABU','WEBL']
 # 초기 금액
-init_cash = 10000
+init_cash = 100000
 # 백테스트 시작 날짜
-back_test_start_date = '2008-02-01'
+back_test_start_date = '2021-09-01'
 # 백테스트 기간 (1년 평균 : 240)
-test_priod = 240 
+test_priod = 480 
 # 평단가 이하 경우 매수 전략 (1: 하루치 전향 매수) 해당 수치 곱하기 개념
-under_price_buy_strategy = 1
-# 평단가 이상 경우 매수 전략 (2: 하루치 절반 매수) 해당 수치 나누기 개념
-upper_price_buy_strategy = 2
+under_price_buy_strategy = 2
+# 평단가 이상 경우 매수 전략 (0.5: 하루치 절반 매수) 해당 수치 곱하기 개념
+upper_price_buy_strategy = 0
+# 목표 수익률 매도 (1.1: 10% 수익일 경우 매도)
+aim_plus_ratio = 1.1
 # 40회차후 매도 전략 (1.05 : 5% 수익시 매도)
-after_40th_buy_strategy = 1.1
+after_40th_buy_strategy = 1.05
 ########################################################
 
 
 ### 백테스트 시작
-def start_back_test (data):
+def start_back_test (stock, data):
     
     # 컬럼 : 날짜, 잔액, 주식명, 거래회차, 평균단가, 보유수량, 현재가, 전체평가액
     # columns : date, cash, stock, nth, avg_px, hold_nm, cur_px, est_val
@@ -43,7 +45,8 @@ def start_back_test (data):
                         ,'hold_nm' : [0]
                         , 'cur_px' : [0]
                         , 'est_val' : [0]})
-    # print(score)
+    
+    # 전날에 다음날 거래를 위해 세팅되는 값
     cur_score = pd.Series({'date' : back_test_start_date
                         ,'cash' : init_cash
                         ,'stock' : stock
@@ -52,8 +55,6 @@ def start_back_test (data):
                         ,'hold_nm' : 0
                         , 'cur_px' : 0
                         , 'est_val' : 0})
-    # print(cur_score)
-
 
     # 하루 매수 최대치 수량
     day_lim_vol = cur_score.cash / 40
@@ -119,8 +120,8 @@ def start_back_test (data):
                 
         # 40회차 이내 경우
         else:
-            # 10% 이득 일때 전량 매도
-            if _avg_px * 1.1 <= _cur_px :
+            # 목표수익률 일때 전량 매도
+            if _avg_px * aim_plus_ratio <= _cur_px :
     #             print("\n 2 -----------------------------------------------")
                 _before_cash = cur_score.cash
                 _after_cash = _before_cash + (_cur_px * _hold_nm)
@@ -134,7 +135,6 @@ def start_back_test (data):
                 # 현재가 < 평단 경우 그날 한계 수량 매수
                 if _cur_px < _avg_px:
     #                 print("\n 3 -----------------------------------------------")
-                    # 모두 매수
                     _buy_nm = int(day_lim_vol * under_price_buy_strategy / _cur_px)
                     _buy_vol = _buy_nm * _cur_px
                     _before_cash = cur_score.cash
@@ -147,8 +147,7 @@ def start_back_test (data):
                 # 평단이 현재가 보다 높을 경우 절반 매수
                 else :
     #                 print("\n 4 -----------------------------------------------")
-                    # 절반 매수
-                    _buy_nm = int(day_lim_vol / upper_price_buy_strategy / _cur_px)
+                    _buy_nm = int(day_lim_vol * upper_price_buy_strategy / _cur_px)
                     _buy_vol = _buy_nm * _cur_px
                     _before_cash = cur_score.cash
                     _nth += 1
@@ -159,19 +158,20 @@ def start_back_test (data):
             
         new_score = {'date' : _date
                     ,'cash' : _after_cash
-                        ,'stock' : 'TQQQ'
-                        ,'nth' : _nth
-                        ,'avg_px' : _avg_px
-                        ,'hold_nm' : _hold_nm
-                        , 'cur_px' : _cur_px
-                        , 'est_val' : _est_val
-                        }
+                    ,'stock' : stock
+                    ,'nth' : _nth
+                    ,'avg_px' : _avg_px
+                    ,'hold_nm' : _hold_nm
+                    , 'cur_px' : _cur_px
+                    , 'est_val' : _est_val
+                    }
         score = score.append(new_score, ignore_index=True)
     #     print(score)
         
+        # 다음거래를 위해 설정
         cur_score.date = _date
         cur_score.cash = _after_cash
-        cur_score.stock ='TQQQ'
+        cur_score.stock = stock
         cur_score.nth = _nth
         cur_score.avg_px = _avg_px
         cur_score.hold_nm = _hold_nm
@@ -186,6 +186,7 @@ def start_back_test (data):
         if i+1 == len(data) :
             break
 
+    # TODO : yahoo 에서 가지고 오는 날짜 값 확인 필요
 
     # 그래프 그리기
     fig, ax = plt.subplots()
@@ -198,9 +199,9 @@ def start_back_test (data):
     # placed on the right by twinx above.
     twin2.spines['right'].set_position(("axes", 1.1))
 
-    p1, = ax.plot(score.est_val[1:], "b-", label=stock+' Evaluation')
-    p2, = twin1.plot(score.cur_px[1:], "r-", label=stock+' Stock_val')
-    p3, = twin2.plot(score.nth[1:], "g--", label=stock+' Nth', linestyle='--')
+    p1, = ax.plot(score.est_val[1:], "b-")
+    p2, = twin1.plot(score.cur_px[1:], "r-")
+    p3, = twin2.plot(score.nth[1:], "g--", linestyle='--')
 
     ax.set_xlabel("Day")
     ax.set_ylabel("Evaluation")
@@ -216,33 +217,44 @@ def start_back_test (data):
     twin1.tick_params(axis='y', colors=p2.get_color(), **tkw)
     twin2.tick_params(axis='y', colors=p3.get_color(), **tkw)
     ax.tick_params(axis='x', **tkw)
+    ax.set_title(stock)
 
     ax.legend(handles=[p1, p2, p3])
 
     plt.show()
 
     ### 엑셀저장
-    # plt.savefig("myplot.png", dpi=150)
+    plt.savefig("myplot.png", dpi=150)
 
-    # wb = openpyxl.load_workbook('C:/Users/jseung/Quant-master/FIRE/input.xlsx')
-    # ws = wb.active
-
-    # img = openpyxl.drawing.image('myplot.png')
-    # img.anchor(ws.cell('A1'))
-
-    # ws.add_image(img)
-    # wb.save('C:/Users/jseung/Quant-master/FIRE/output.xlsx')
 
 
 ### init
+
+# 엑셀저장
+# wb = openpyxl.load_workbook('C:/Users/jseung/Quant-master/FIRE/input.xlsx')
+wb = openpyxl.Workbook()
+ws = wb.worksheets[0]
+
+cell_row = 1
+
+
 for stock in stock_list :
 
     # 주식 데이터 가지고 오기
-    data = yf.download(stock,start = back_test_start_date)
-    data=data.reset_index()
+    data = yf.download(stock, start = back_test_start_date)
+    data = data.reset_index()
 
     # 백테스트 시작
-    start_back_test(data)
+    start_back_test(stock, data)
+
+    #
+    img = openpyxl.drawing.image.Image('myplot.png')
+    img.anchor = 'A'+str(cell_row)
+    cell_row += 35
+    ws.add_image(img)
+
+
+wb.save('C:/Users/jseung/Quant-master/FIRE/output.xlsx')
 
 
 
